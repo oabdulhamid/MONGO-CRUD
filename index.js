9a0d3d8b90e8d28fa96cc0b47d4101321afe2341
oabdulhamid <oabdulhamid1@gmail.com>
2022-11-15 15:34:55 +0000

    Todo API

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.use("/static", express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+//view engine configuration
+app.set("view engine", "ejs");
+
+app.get('/',(req, res) => {
+    res.render('todo.ejs');
+    });
+    
+app.post('/',(req, res) => {
+    console.log(req.body);
+    });
+
+app.listen(3000, () => console.log("Server Up and running"));
