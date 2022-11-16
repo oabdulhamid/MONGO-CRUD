03e5f24c0b25d9130ad5131519ff7e20589fe347
oabdulhamid <oabdulhamid1@gmail.com>
2022-11-16 09:02:10 +0000

    Updated Todo App

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const TodoTask = require("./models/TodoTask");
+
 const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.use("/static", express.static("public"));
 
@@ -8,12 +14,56 @@ app.use(express.urlencoded({ extended: true }));
 //view engine configuration
 app.set("view engine", "ejs");
 
-app.get('/',(req, res) => {
-    res.render('todo.ejs');
+// GET METHOD
+app.get("/", (req, res) => {
+    TodoTask.find({}, (err, tasks) => {
+        res.render("todo.ejs", { todoTasks: tasks });
     });
-    
-app.post('/',(req, res) => {
-    console.log(req.body);
+});
+
+// POST METHOD
+app.post('/', async (req, res) => {
+    const todoTask = new TodoTask({
+        content: req.body.content
+    });
+    try {
+        await todoTask.save();
+        res.redirect("/");
+    } catch (err) {
+        res.redirect("/");
+    }
+});
+
+// UPDATE METHOD
+app
+    .route("/edit/:id")
+    .get((req, res) => {
+        const id = req.params.id;
+        TodoTask.find({}, (err, tasks) => {
+            res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+        });
+    })
+    .post((req, res) => {
+        const id = req.params.id;
+        TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+            if (err) return res.send(500, err);
+            res.redirect("/");
+        });
     });
 
-app.listen(3000, () => console.log("Server Up and running"));
+//DELETE
+app.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndRemove(id, err => {
+        if (err) return res.send(500, err);
+        res.redirect("/");
+    });
+});
+// app.listen(3000, () => console.log("Server Up and running"));
+
+//connection to db
+// mongoose.set("useFindAndModify", false);
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+    console.log("Connected to db!");
+    app.listen(3000, () => console.log("Server Up and running"));
+});
